6aa170c6d49c621e307a528f2bc6a7e0bb874128
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-11-06 22:35:37 +0300

    updated connection to mongoDB

--- main.go
+++ main.go
@@ -29,9 +29,12 @@ var mongoClient *mongo.Client
 func initMongo() {
 	var err error
 	mongoURI := os.Getenv("MONGO_URL")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
 	mongoClient, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		log.Fatal("Mongo conneciton error:", err)
+		log.Fatal("Mongo connection error:", err)
 	}
 }
 
